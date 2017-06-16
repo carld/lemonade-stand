4d16d59b4b131f632729c15ba1de49d2c4f84306
katee <hello@kate.io>
2017-06-16 16:14:23 -0400

    Add introduction and start game question

--- src/lemonade_stand/core.clj
+++ src/lemonade_stand/core.clj
@@ -1,7 +1,26 @@
 (ns lemonade-stand.core
+  (:require [lemonade-stand.screen :as screen])
   (:gen-class))
+
+(defn print-introduction []
+  (screen/clear)
+  (screen/move-top)
+  (println "Lemonade Stand")
+  (println "")
+  (println "Hi! Would you like to start a new game? (yes or no)")
+  )
+
+(defn start-game-question []
+  (loop []
+    (println "Type your answer and hit return ==>")
+    (let [start-game (read-line)]
+      (if (and (not (.startsWith start-game "y")) (not (.startsWith start-game "n")))
+          (do (screen/bell) (recur))
+          (println "we left the loop")))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (print-introduction)
+  (start-game-question)
+  )
