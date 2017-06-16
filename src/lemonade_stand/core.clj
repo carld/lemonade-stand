3c064da5a26655c8bbab4823df06803354882f62
Carl <carl.douglas@gmail.com>
2017-06-16 17:26:54 -0400

    start of new day

--- src/lemonade_stand/core.clj
+++ src/lemonade_stand/core.clj
@@ -4,6 +4,7 @@
 
 (def day 0)
 (def sky-color 0)
+(def cost 0)
 
 (defn print-introduction []
   (screen/clear)
@@ -21,26 +22,29 @@
           (do (screen/bell) (recur))
           (println "we left the loop")))))
 
+;;; Line 400
 (defn weather-report []
-                                        ; a random number from 0 to 0.999
+ ;;; a random number from 0 to 0.999
   (set! sky-color (rand))
   (let []
     (cond
       (< 0.6 sky-color)  (set! sky-color 2)
       (< 0.8 sky-color)  (set! sky-color 10)
       :else (set! sky-color 7))
+
     (if (< 3 day)
       (set! sky-color 2))
     ))
 
+;;; Line 15000
 (defn weather-display []
   (screen/clear)
   (println "Weather Report")
   (case sky-color
-    2 (println "   Sunny")
-    7 (println "   Hot and dry")
+    2  (println "   Sunny")
+    7  (println "   Hot and dry")
     10 (println "   Cloudy")
-    5 (println "Thunderstorms")
+    5  (println "   Thunderstorms")
     )
   (println "")
   (println "Press space to continue, escape to end")
@@ -49,6 +53,19 @@
       (if (and (not (.startsWith in " ")) (not (.startsWith in 27)))
         (recur))
       ))
+  )
+
+;;; Line 500
+(defn start-of-new-day []
+  (set! day (+ 1 day))
+  (format "On day ~d, the cost of lemonade is ")
+  (set! cost 2)
+  (if (> 2 day)
+    (set! cost 4))
+  (if (> 6 day)
+    (set! cost 5))
+  (format "$0.0%d" cost)
+
   )
 
 (defn -main
