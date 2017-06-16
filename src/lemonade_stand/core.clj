8e1b085298cbb0470f1eed835e8bb686cb499913
Carl <carl.douglas@gmail.com>
2017-06-16 17:12:03 -0400

    weather report and weather display functions

--- src/lemonade_stand/core.clj
+++ src/lemonade_stand/core.clj
@@ -2,6 +2,9 @@
   (:require [lemonade-stand.screen :as screen])
   (:gen-class))
 
+(def day 0)
+(def sky-color 0)
+
 (defn print-introduction []
   (screen/clear)
   (screen/move-top)
@@ -17,6 +20,36 @@
       (if (and (not (.startsWith start-game "y")) (not (.startsWith start-game "n")))
           (do (screen/bell) (recur))
           (println "we left the loop")))))
+
+(defn weather-report []
+                                        ; a random number from 0 to 0.999
+  (set! sky-color (rand))
+  (let []
+    (cond
+      (< 0.6 sky-color)  (set! sky-color 2)
+      (< 0.8 sky-color)  (set! sky-color 10)
+      :else (set! sky-color 7))
+    (if (< 3 day)
+      (set! sky-color 2))
+    ))
+
+(defn weather-display []
+  (screen/clear)
+  (println "Weather Report")
+  (case sky-color
+    2 (println "   Sunny")
+    7 (println "   Hot and dry")
+    10 (println "   Cloudy")
+    5 (println "Thunderstorms")
+    )
+  (println "")
+  (println "Press space to continue, escape to end")
+  (loop []
+    (let [in (read-line)]
+      (if (and (not (.startsWith in " ")) (not (.startsWith in 27)))
+        (recur))
+      ))
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
